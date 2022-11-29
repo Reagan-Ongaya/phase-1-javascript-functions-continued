c007949610d175db25ecb2d2327800d2e282c31e
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-29 12:21:48 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function greeting() {
+    console.log("Hello Fellow Humans!, we are going to work with numbers and strings today.");
+}
+
+greeting();
+
+function numbers(param1,param2){
+    console.log(param1 + param2)
+}
+
+numbers("5",5);
+numbers( 5 , 5)
